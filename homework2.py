f80b5010bbf0a2a0a8588fbb9382beb66f7c5d2e
Irina <fransik@bk.ru>
2023-05-31 19:53:20 +0300

    guess numbers by sum and product

--- homework2.py
+++ homework2.py
@@ -23,12 +23,12 @@ if heads < tails:
 # а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 # Помогите Кате отгадать задуманные Петей числа.
 
-# S = int(input('The sum of the numbers X and Y is \n'))
-# P = int(input('The product of the numbers X and Y is \n'))
-# for X in range(S):
-#     for Y in range(P):
-#         if S == X + Y and P == X * Y:
-#             print(f'The first number is "{X}", the second number is "{Y}"')
+S = int(input('The sum of the numbers X and Y is \n'))
+P = int(input('The product of the numbers X and Y is \n'))
+for X in range(S):
+    for Y in range(P):
+        if S == X + Y and P == X * Y:
+            print(f'The first number is "{X}", the second number is "{Y}"')
 
 
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
