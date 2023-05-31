72a76f6b2c497c0681cb490331c3d43bc7b7a6aa
Irina <fransik@bk.ru>
2023-05-31 20:14:53 +0300

    unique values in the dictionary

--- seminar3.py
+++ seminar3.py
@@ -50,67 +50,41 @@ print(data)
 #  0   1   2   3   4
 
 data = [[1, -1.575], ['Hello, world', True], [15, -7]]
-# # #           0                  1                 2
+# #           0                  1                 2
 print(data[2][0])
 
 data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
 # # key: value
 print(data['Konstantin']['hobby'][0])
-
-
-
-# data = [[1, -1.575], ['Hello, world', True], [15, -7]]
-# #           0                  1                 2
-# print(data[2][0])
-
-# data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
-# # key: value
-# print(data['Konstantin']['hobby'][0])
 # # .values()
 # # .keys()
-# print(list(data.values()))
-# print(list(data.keys()))
-
-
-
-
-# data = [[1, -1.575], ['Hello, world', True], [15, -7]]
-# #           0                  1                 2
-# print(data[2][0])
-
-# data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
-# # key: value
-# print(data['Konstantin']['hobby'][0])
-# # .values()
-# # .keys()
-# print(list(data.values()))
-# print(list(data.keys()))
-# print(list(data.items()))
-# for k, v in data.items():
-#     print(k, v)
-
+print(list(data.values()))
+print(list(data.keys()))
+print(list(data.items()))
+for k, v in data.items():
+    print(k, v)
 
 # Напишите программу для печати всех уникальных значений в словаре. 
 # Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}] 
 # Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 
-# data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
-#          {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
-#            {"VIII":"S007"}]
-# result = set()
-# for item in data:
-#     result.add(list(item.values())[0])
-# print(result)
+data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+         {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
+           {"VIII":"S007"}]
+result = set()
+for item in data:
+    result.add(list(item.values())[0])
+print(result)
 
-# вариант 2
-# data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
-#          {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
-#            {"VIII":"S007"}]
-# result = set()
-# for item in data:
-#     for key in item:
-#         result.add(item[key])
-# print(result)
+# variant 2
+data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+         {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
+           {"VIII":"S007"}]
+result = set()
+for item in data:
+    for key in item:
+        result.add(item[key])
+print(result)
 
 
 # Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером) 
