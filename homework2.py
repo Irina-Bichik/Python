da8c2c85090d0814d8ea45c1543bb3fc620735c8
Irina <fransik@bk.ru>
2023-05-31 19:51:29 +0300

    the task about coins

---
+++ homework2.py
@@ -0,0 +1,42 @@
+# Циклы (for, while)
+
+# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
+# чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть.
+
+n = int(input('Input the number of coins: '))
+heads = 0
+tails = 0
+for i in range(n):
+    count = int(input('Input - how the coin lies - up heads (1) or tails (2)? '))
+    if count == 1:
+        heads += 1
+    else:
+        tails += 1
+if heads > tails:
+    print(f'Minimum number of coins to be flipped is {tails} ')
+if heads == tails:
+    print(f'No need to flip the coins')
+if heads < tails:
+    print(f'Minimum number of coins to be flipped is {heads}')
+
+# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000), 
+# а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
+# Помогите Кате отгадать задуманные Петей числа.
+
+# S = int(input('The sum of the numbers X and Y is \n'))
+# P = int(input('The product of the numbers X and Y is \n'))
+# for X in range(S):
+#     for Y in range(P):
+#         if S == X + Y and P == X * Y:
+#             print(f'The first number is "{X}", the second number is "{Y}"')
+
+
+# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+# N = int(input('Input the number N: '))
+# k=1
+# while k<=N:
+#     print(k,end=' ')
+#     k=2*k
+
+
---
+++ seminar3.py
@@ -0,0 +1,142 @@
+# data = [int(i) for i in input("Введите числа: ").split()]
+# print(data)
+
+# data = [int(i) for i in input("Введите числа: ").split()]
+# print(data)
+
+# list_1 = [1, -1.575, 'Hello, world!', True]
+# list
+# array(import array || import numpy)
+# t = (1, 2)
+# t[0] += 1
+# tuple 
+# dict
+# set
+# set_1 = {1, 2, 3, 4}
+# set_1.add(56)
+# print(*set_1)
+
+# list() - set() - tuple()
+
+# Дан список чисел. Определите, сколько в нем встречается различных чисел.
+# Input: [1, 1, 2, 0, -1, 3, 4, 4]
+# Output: 6
+
+# data = [int(i) for i in input("Введите числа: ").split()]
+# print(len(set(data)))
+
+# Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность 
+# (сдвиг - циклический) на K элементов вправо,  K – положительное число.
+# Input:   [1, 2, 3, 4, 5] k = 3
+# Output:  [3, 4, 5, 1, 2]
+
+# data = [int(i) for i in input("Введите числа: ").split()]
+# steps = int(input("Введите кол-во сдвигов: "))
+# steps = steps % len(data)
+# data = [data[i - steps] for i in range(len(data))]
+# print(data)
+
+# i = 0: data[0 - 3]
+# i = 1: data[1 - 3]
+# i = 2: data[2 - 3]
+# i = 3: data[3 - 3]
+# i = 4: data[4 - 3]
+
+# [10, 20, 30, 40, 50]
+#  -5  -4  -3  -2  -1
+# steps = 3
+# [30, 40, 50, 10, 20]
+#  -3  -2  -1  -5  -4
+#  0   1   2   3   4
+
+# data = [[1, -1.575], ['Hello, world', True], [15, -7]]
+# #           0                  1                 2
+# print(data[2][0])
+
+# data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
+# # key: value
+# print(data['Konstantin']['hobby'][0])
+
+
+
+# data = [[1, -1.575], ['Hello, world', True], [15, -7]]
+# #           0                  1                 2
+# print(data[2][0])
+
+# data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
+# # key: value
+# print(data['Konstantin']['hobby'][0])
+# # .values()
+# # .keys()
+# print(list(data.values()))
+# print(list(data.keys()))
+
+
+
+
+# data = [[1, -1.575], ['Hello, world', True], [15, -7]]
+# #           0                  1                 2
+# print(data[2][0])
+
+# data = {"Ivan": 27, 'Alexandr': 36, 'Konstantin': {'age': 21, 'hobby': ['tennis', 'fotball']}}
+# # key: value
+# print(data['Konstantin']['hobby'][0])
+# # .values()
+# # .keys()
+# print(list(data.values()))
+# print(list(data.keys()))
+# print(list(data.items()))
+# for k, v in data.items():
+#     print(k, v)
+
+
+# Напишите программу для печати всех уникальных значений в словаре. 
+# Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}] 
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+# data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+#          {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
+#            {"VIII":"S007"}]
+# result = set()
+# for item in data:
+#     result.add(list(item.values())[0])
+# print(result)
+
+# вариант 2
+# data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+#          {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"},
+#            {"VIII":"S007"}]
+# result = set()
+# for item in data:
+#     for key in item:
+#         result.add(item[key])
+# print(result)
+
+
+# Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером) 
+# Input: [0, -1, 5, 2, 3]
+# Output: 2 
+# Пояснение: (-1 < 5, 2 < 3)
+
+# data = [int(i) for i in input("Введите числа: ").split()]
+# count = 0
+# for i in range(len(data) - 1):
+#     if data[i + 1] > data[i]:
+#         count += 1
+# print(count)
+
+
+
+
+# data = [int(i) for i in input("Введите числа: ").split()]
+# count = 0
+# for i in range(len(data) - 1):
+#     if data[i + 1] > data[i]:
+#         count += 1
+# print(count)
+# n = 10
+# print(int(n > 5))
+# True - 1
+# False - 0
+data = [int(i) for i in input("Введите числа: ").split()]
+print(sum([int(data[i + 1] > data[i]) for i in range(len(data) - 1)]))
